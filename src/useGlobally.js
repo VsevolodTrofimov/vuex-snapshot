22110952ca8e1e4720e7d8e758238d7dea706e25
v-trof <seva.trofimov2@gmail.com>
2018-02-03 03:29:59 +0300

    Exported useGlobally by default

--- src/useGlobally.js
+++ src/useGlobally.js
@@ -1,4 +1,6 @@
-export const useGlobally = (name, value) => {
+const useGlobally = (name, value) => {
   if(typeof window === 'undefined') global[name] = value // node
   else window[name] = value // browser
 }
+
+export default useGlobally
