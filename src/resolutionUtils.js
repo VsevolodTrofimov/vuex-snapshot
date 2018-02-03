d2fb2bdd7cab2e50ac996952f64ed2f2dbfa1c62
v-trof <seva.trofimov2@gmail.com>
2018-02-03 03:33:22 +0300

    Created snapAction

---
+++ src/resolutionUtils.js
@@ -0,0 +1,61 @@
+import serialize from './serialize'
+
+
+export const normalizeResolution = resolution => {
+  const normalResolution = {
+    name: '',
+    type: 'resolve',
+    payload: undefined,
+  }
+  
+  // string constructor
+  if(typeof resolution === 'string' || resolution.indexOf('string')) {
+    normalResolution.name = resolution
+    return normalResolution
+  } 
+  
+  // object constructor, errors are duplicated because they are likely to occur in promises
+  if(resolution.name) {
+    normalResolution.name = resolution.name
+  } else {
+    throw new Error('vuex-snapshot: INPUT ERROR resolution must have a name')
+  }
+
+  if(resolution.type && ['resolve', 'reject'].indexOf(resolution.type) !== -1) {
+    normalResolution.type = resolution.type
+  } else {
+    throw new Error('vuex-snapshot: INPUT ERROR resolution type must be' 
+                    + 'either "resovle" or "reject"')
+  }
+
+  if(resolution.payload) {
+    normalResolution.payload = resolution.payload
+  }
+}
+
+
+export const simualteResolution = (resolution, snapCb, timetable) => {
+  snapCb(`RESOLUTION: ${resolution.name} -> ${resolution.type}`, resolution.payload)
+  return timetable.trigger(resolution)
+}
+
+
+export const simualteResolutions = (resolutions, snapCb, timetable) => {
+  return new Promise((resolveSimulation, rejectSimulation) => {
+    const normalResolutions = resolutions.map(normalizeResolution)
+    
+    // simulates given resolution and queues the next until all are simulated
+    const simulationLoop = idx => {
+      if(idx === normalResolutions.length) {
+        resolveSimulation()
+      }
+      else {
+        simualteResolution(normalResolutions[idx], snapCb, timetable)
+          .then(() => simulationLoop(idx + 1))
+          .catch(rejectSimulation)
+      }
+    }
+
+    simulationLoop(0)
+  })
+}
---
+++ src/snapAction.js
@@ -0,0 +1,91 @@
+import serialize from './serialize'
+import timetable from './timetable'
+import {simualteResolutions} from './resolutionUtils'
+
+
+const RealPromise = Promise
+
+
+export class Snapshot {
+  constructor() {
+    this.value = ''
+    this.add = this.add.bind(this)
+  }
+  add(message, payload) {
+    this.value += message 
+    if(typeof payload !== 'undefined') this.value += '\n' + serialize(payload)
+    this.value += '\n---\n'
+  }
+}
+
+
+export const makeCallSnapper = (snapshot, type) => (name, payload) => {
+  snapshot.add(`${type}: ${name}`, payload)
+}
+
+/**
+ * @typedef {{name:string, type: ("resolve" | "reject"), payload}} Resolution
+ */
+/**
+ * Takes snapshot of action's evaluation
+ * @param {Function} action action to test
+ * @param {{state, getters, payload}} mocks what should be in the store when action runs & action argument
+ * @param {[(string | Resolution)]} resolutions
+ * @returns  {(string | Promise<string>)}
+ */
+export const snapAction = (action, mocks={}, resolutions=[]) => {
+  // for 2 arg call (action, resolutions)
+  if(Array.isArray(mocks)) {
+    resolutions = mocks
+    mocks = {}
+  }
+
+  const snapshot = new Snapshot()
+  const mockCommit = makeCallSnapper(snapshot, 'COMMIT')
+  const mockDispatch = makeCallSnapper(snapshot, 'DISPATCH')
+
+
+  const actionReturn = action({
+    commit: mockCommit,
+    dispatch: mockDispatch,
+    state: mocks.state,
+    getters: mocks.getters
+  }, mocks.payload)
+
+
+  if(typeof actionReturn !== 'undefined' && actionReturn instanceof Promise) {
+    // action is async
+    return new RealPromise((resolve, reject) => {
+      actionReturn
+        .then(payload => {
+          snapshot.add('ACTION RESOLVED', payload)
+          resolve(snapshot.value)
+        })
+        .catch(payload => {
+          snapshot.add('ACTION REJECTED', payload)
+          resolve(snapshot.value)
+        })
+      
+      simualteResolutions(resolutions, snapshot.add, timetable)
+        .then(() => {
+          // this is needed to let action to resolve first
+          setTimeout(() => {
+            snapshot.add('ACTION DID NOT RESOLVE')
+            resolve(snapshot.value)
+          }, 0)
+        })
+        .catch(err => {
+          reject({
+            err,
+            snapshot
+          })
+        })
+
+    })
+  } else {
+    // action is sync
+    return snapshot.value
+  }
+}
+
+export default snapAction
