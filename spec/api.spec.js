dec934d89dd25f111ac780f4afb1327d73835e7a
v-trof <seva.trofimov2@gmail.com>
2018-02-06 06:40:27 +0300

    Added snapshot tests to api spec

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -1,3 +1,6 @@
+import * as promiseLib from '../src/mockPromise'
+import * as fetchLib from '../src/mockFetch'
+
 // fancy mocking
 const snapActionModule = require('../src/snapAction')
 const snapActionCore = snapActionModule.default
@@ -7,6 +10,7 @@ snapActionModule.default = snapAction
 // the ones being tested
 import vuexSnapshot from '../index'
 import config from '../src/config'
+import { MockPromise } from '../src/mockPromise';
 
 
 describe('API', () => {
@@ -36,17 +40,25 @@ describe('API', () => {
     it('Resets all w/ .reset', () => {
       const realTimetableReset = vuexSnapshot.timetable.reset
       const realConfigReset = config.reset
+      const realUseRealPromise = promiseLib.useReal
+      const realUseRealFetch = fetchLib.useReal
       
       vuexSnapshot.timetable.reset = jest.fn()
       config.reset = jest.fn()
+      promiseLib.useReal = jest.fn()
+      fetchLib.useReal = jest.fn()
     
       vuexSnapshot.reset()
     
       expect(vuexSnapshot.timetable.reset).toBeCalled()
       expect(config.reset).toBeCalled()
+      expect(promiseLib.useReal).toBeCalled()
+      expect(fetchLib.useReal).toBeCalled()
     
       vuexSnapshot.timetable.reset = realTimetableReset
       config.reset = realConfigReset
+      promiseLib.useReal = realUseRealPromise
+      fetchLib.useReal = realUseRealFetch
     })
   })
 
@@ -152,28 +164,164 @@ describe('API', () => {
 
 
   describe('Snapshots (backwards compability check)', () => {
+    beforeEach(vuexSnapshot.reset)
+
     it('Sync action', () => {
+      const action = ({commit, dispatch}) => {
+        commit('increment', 10)
+        commit('decrement')
+        dispatch('something useful', {notThisOne: 'yeah'})
+      }
 
+      expect(vuexSnapshot.snapAction(action)).toMatchSnapshot()
     })
 
-    it('Async simple', () => {
-      
+    it('Sync action w/ env & payload', () => {
+      const action = ({commit, dispatch, state, getters}, payload) => {
+        commit('increment', state.incBig)
+        commit('decrement', getters.sadness)
+        dispatch('something useful', payload)
+      }
+
+      const mocks = {
+        state: {
+          irrelevat: false,
+          incBig: 100
+        },
+        getters: {sadness: Infinity},
+        payload: {action}
+      }
+
+      vuexSnapshot.config.snapEnv = true
+
+      expect(vuexSnapshot.snapAction(action, mocks)).toMatchSnapshot()
     })
 
-    it('Async inner promise', () => {
-      
+    it('Async simple', done => {
+      const action = ({commit}) => {
+        commit('smth')
+        return Promise.resolve()
+      }
+
+      vuexSnapshot.snapAction(action)
+        .then(run => {
+          expect(run).toMatchSnapshot()
+          done()
+        })
+        .catch(done)
     })
 
-    it('Async state or getter promise', () => {
-      
+    it('Async inner promise', done => {
+      vuexSnapshot.useMockPromise()
+
+      const action = ({commit}) => new Promise(resolve => {
+        commit('smth')
+        new Promise(() => {}).then(data => {
+          commit('use', data)
+          resolve(data)
+        })
+      })
+
+      vuexSnapshot.snapAction(action, ['Promise'])
+        .then(run => {
+          expect(run).toMatchSnapshot()
+          done()
+        })
+        .catch(done)
     })
 
-    it('Async fetch', () => {
-    
+    it('Async state or getter promise', done => {
+      const action = ({commit, state}) => new Promise(resolve => {
+        commit('smth')
+        state.preloadPromise.then(data => {
+          commit('use', data)
+          resolve(data)
+        })
+      })
+
+      const state = {
+        preloadPromise: new vuexSnapshot.MockPromise('preload')
+      }
+
+      vuexSnapshot.snapAction(action, {state}, [{name: 'preload', payload: 'a lot of data'}])
+        .then(run => {
+          expect(run).toMatchSnapshot()
+          done()
+        })
+        .catch(done)
     })
 
-    it('Async timeout', () => {
-      
+    it('Async fetch', done => {
+      vuexSnapshot.useMockFetch()
+
+      const action = ({dispatch}, attempts=0) => new Promise((resolve, reject) => {
+        const request = fetch('http://api.example.com/whatever')
+        request
+          .then(response => response.json())
+          .then(json => {
+            dispatch('useExample', json)
+            resolve()
+          })
+          .catch(message => {
+            dispatch('callExampleAPI', attempts + 1)
+            reject()
+          })
+
+        return request
+      })
+
+      const resolutions = [{
+        name: 'http://api.example.com/whatever',
+        payload: {
+          json: () => new MockPromise('json')
+        }
+      }, {
+        name: 'json',
+        payload: {
+          data: 'yep',
+          important: true
+        }
+      }]
+
+      vuexSnapshot.snapAction(action, resolutions)
+        .then(run => {
+          expect(run).toMatchSnapshot()
+          done()
+        })
+        .catch(err => {
+          console.error(err)
+          done(err)
+        })
+
+      console.log(vuexSnapshot.timetable.entries)
+    })
+
+    it('Async rejected', done => {
+      vuexSnapshot.config.allowManualActionResolution = true
+      vuexSnapshot.useMockFetch()
+
+      const action = ({dispatch}, attempts=0) => {
+        const request = fetch('http://api.example.com/whatever')
+        
+        request.catch(message => {
+          dispatch('callExampleAPI', attempts + 1)
+        })
+
+        return request
+      }
+
+      const resolutions = [{
+        name: 'http://api.example.com/whatever',
+        type: 'reject',
+        payload: new TypeError('Failed to fetch')
+      }]
+
+      vuexSnapshot.snapAction(action, resolutions)
+        .then(run => {
+          expect(run).toMatchSnapshot()
+          done()
+        })
+        .catch(done)
     })
   })
 })
